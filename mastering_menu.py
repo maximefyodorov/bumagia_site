b863a9ad17900bb362cdf0569ccbaf3b3279cf89
maximefyodorov <maxime.fyodorov@gmail.com>
2020-09-11 12:03:52 +0300

    Last stable version

---
+++ mastering_menu.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+import jinja2
+import openpyxl
+
+wb = openpyxl.load_workbook('w:/home/bumagia/www/templates/bumagia_data.xlsx', data_only=True)
+
+products_list = wb.sheetnames[1:]
+number_of_products = len(products_list)
+
+ws = wb['common_data']
+
+my_menuitem_name =[]
+
+templates_list = [
+    'bookmarks',
+    'modul_origami',
+    'papercubes',
+    'puzzles',
+    'quilling',
+    'quilling3d',
+    'quilmagnet',
+    'sketchbooks',
+    'twosidespaper',
+    'sertification',
+    'instructions',
+    'videolessons' 
+]
+
+link_prefix_list = [
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    'products/',
+    '../products/',
+    '../products/' 
+]
+
+path_prefix_list = [
+    'products/',
+    'products/',
+    'products/',
+    'products/',
+    'products/',
+    'products/',
+    'products/',
+    'products/',
+    'products/',
+    '',
+    'learn/',
+    'learn/' 
+]
+
+
+for i in range (2, ws.max_row+1):
+    my_menuitem_name.append(ws.cell(row = 8, column = i).value)
+
+templateLoader = jinja2.FileSystemLoader(searchpath='w:/home/bumagia/www/templates/Inner_templates/')
+templateEnv = jinja2.Environment(loader=templateLoader)
+for i in (range(len(templates_list))):
+    tmplt = templateEnv.get_template('{}_template.html'.format(templates_list[i]))
+    with open('w:/home/bumagia/www/{}{}.html'.format(path_prefix_list[i], templates_list[i]), 'wb') as dest:
+        output = tmplt.render(
+            p_list = products_list,
+            p_num = number_of_products,
+            menuname_list = my_menuitem_name,
+            prefix = link_prefix_list[i]
+        )
+        dest.write(output.encode('utf-8'))
---
+++ mastering_pages.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+
+import jinja2
+import openpyxl
+
+wb = openpyxl.load_workbook('w:/home/bumagia/www/templates/bumagia_data.xlsx', data_only=True)
+
+products_list = wb.sheetnames[1:]
+number_of_products = len(products_list)
+
+ws = wb['common_data']
+
+products_data = {}
+my_products = []
+my_menuitem_name =[]
+
+for i in range (number_of_products):
+    if ws.cell(row = 1, column = i+2).value == 1:
+        my_products.append(ws.cell(row = 2, column = i+2).value)
+        temp_dict = {}
+        for j in range (2, ws.max_row+1):
+            temp_dict[ws.cell(row = j, column = 1).value] = ws.cell(row = j, column = i+2).value
+        products_data[products_list[i]] = temp_dict
+
+for i in range (2, ws.max_row+1):
+    my_menuitem_name.append(ws.cell(row = 8, column = i).value)
+
+for my_product in (my_products):
+
+    ws = wb[my_product]
+
+    my_arr_name = []
+    my_arr_articul = []
+    my_arr_full_articul = []
+    my_arr_mame_and_articul = []
+    my_arr_buylink = []
+
+    my_len = 0
+
+    for i in range(1, ws.max_row + 1):
+        # Управляющая колонка 1 (0 - игнорировать, 1 - выводить в общий список, 2 - выводить на витрину на главной)
+        control = ws.cell(row = i, column = 1).value 
+        if (control != 0):
+            my_len+=1
+            # Название (колонка 2)
+            my_arr_name.append(ws.cell(row = i, column = 2).value)
+            # Артикул без буквенной части - последние четыре цифры штрихкода (колонка 3)
+            my_arr_articul.append(str(ws.cell(row = i, column = 3).value)[9:])
+            # Полный артикул, индекс и цифры (колонка 4)
+            my_arr_full_articul.append(ws.cell(row = i, column = 4).value)
+            # Имя и полный артикул вместе (колонка 5)
+            my_arr_mame_and_articul.append(ws.cell(row = i, column = 5).value)
+            # Ссылка на покупку (колонка 6)
+            my_arr_buylink.append(ws.cell(row = i, column = 6).value)
+
+    templateLoader = jinja2.FileSystemLoader(searchpath='w:/home/bumagia/www/templates/Inner_templates/')
+    templateEnv = jinja2.Environment(loader=templateLoader)
+    tmplt = templateEnv.get_template('{}_template.html'.format(my_product))
+    with open('w:/home/bumagia/www/products/{}.html'.format(my_product), 'wb') as dest:
+        output = tmplt.render(
+            p_list = products_list,
+            p_num = number_of_products,
+            menuname_list = my_menuitem_name,
+            prefix = '',
+            p_data = products_data,
+            i_num = my_len,
+            c_prod = my_product, 
+            name_list = my_arr_name,
+            articul_list = my_arr_articul,
+            full_articul_list = my_arr_full_articul,
+            fullnameandarticul_list = my_arr_mame_and_articul,
+            buylink_list = my_arr_buylink
+        )
+        dest.write(output.encode('utf-8'))
